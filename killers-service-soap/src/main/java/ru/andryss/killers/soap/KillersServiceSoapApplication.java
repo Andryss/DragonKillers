e373378cfa253cced3630a481a4a53e9209e5f6e
Andryss <andr.krivosheev@gmail.com>
2024-12-08 13:28:06 +0300

    Init killer-service soap

---
+++ killers-service-soap/src/main/java/ru/andryss/killers/soap/KillersServiceSoapApplication.java
@@ -0,0 +1,13 @@
+package ru.andryss.killers.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KillersServiceSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KillersServiceSoapApplication.class, args);
+    }
+
+}
